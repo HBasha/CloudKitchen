373c9557da32ab5cdc372b13c9bfb2a442207fff
Basha Himmatgori <31625099+HBasha@users.noreply.github.com>
2026-03-18 19:06:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ fetch('menu.json').then(r => r.json()).then(data => {
 
 function renderCategories(categories) {
   const tabs = document.getElementById('categoryTabs');
-  tabs.innerHTML = `<button class='px-4 py-2 text-lg bg-blue-600 text-white rounded-full' onclick="changeCategory('All')">All</button>`;
+  tabs.innerHTML = `<button class='w-full px-4 py-3 text-lg bg-blue-600 text-white rounded-2xl' onclick="changeCategory('All')">All</button>`;
 
   categories.forEach(cat => {
-    tabs.innerHTML += `<button class='px-4 py-2 text-lg bg-gray-200 rounded-full' onclick="changeCategory('${cat.name}')">${cat.name}</button>`;
+    tabs.innerHTML += `<button class='w-full px-4 py-3 text-lg bg-gray-200 rounded-2xl' onclick="changeCategory('${cat.name}')">${cat.name}</button>`;
   });
 }
 
@@ -39,17 +39,18 @@ function renderMenu() {
     if (selectedCategory !== 'All' && selectedCategory !== cat.name) return;
 
     cat.items.filter(i => i.name.toLowerCase().includes(query)).forEach(item => {
+
       const card = document.createElement('div');
       const available = item.available && isOpen;
 
-      card.className = `bg-white p-6 rounded-2xl shadow-lg ${!available ? 'opacity-40' : ''}`;
+      card.className = `bg-white p-6 rounded-2xl shadow-xl ${!available ? 'opacity-40' : ''}`;
 
       card.innerHTML = `
-        <img src='${item.image}' class='w-full h-48 object-cover rounded-xl'>
-        <h2 class='mt-4 text-2xl font-semibold'>${item.name}</h2>
-        <p class='text-gray-700 text-base leading-relaxed mt-1'>${item.description}</p>
-        <p class='mt-3 font-bold text-xl'>₹${item.price}</p>
-        ${available ? `<a href='${createWhatsAppLink(fullData.whatsappNumber, item.name)}' target='_blank'><button class='mt-3 w-full py-3 bg-green-600 text-white rounded-xl text-lg'>Order on WhatsApp</button></a>` : `<p class='text-red-600 text-lg mt-3'>Unavailable</p>`}
+        <img src='${item.image}' class='w-full h-52 object-cover rounded-xl'>
+        <h2 class='mt-4 text-2xl font-semibold tracking-wide'>${item.name}</h2>
+        <p class='text-gray-700 text-lg leading-relaxed mt-2'>${item.description}</p>
+        <p class='mt-3 font-bold text-2xl'>₹${item.price}</p>
+        ${available ? `<a href='${createWhatsAppLink(fullData.whatsappNumber, item.name)}' target='_blank'><button class='mt-4 w-full py-3 bg-green-600 text-white text-xl rounded-xl'>Order on WhatsApp</button></a>` : `<p class='text-red-600 text-xl mt-3'>Unavailable</p>`}
       `;
 
       container.appendChild(card);
